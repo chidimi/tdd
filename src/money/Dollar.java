b89cee4ccad51fa6e323b7e574d96d57e6a1e9ee
chidimi <daichi.imai@hotmail.com>
2021-01-16 22:52:14 +0900

    chapter2

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,13 +1,13 @@
 package money;
 
 public class Dollar {
-    public int amount = 10;
+    public int amount;
     public Dollar(int amount){
-
+        this.amount = amount;
     }
 
-    public void times(int multiplier) {
-
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 
 }
