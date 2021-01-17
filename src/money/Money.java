aed59063d7ab6f515b8fd2b7d249acd1f75d7d43
chidimi <daichi.imai@hotmail.com>
2021-01-17 18:52:15 +0900

    chapter12

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
     public Money times(int multiplier) {
@@ -32,6 +32,10 @@ public class Money {
 
     public String toString() {
         return amount + " " + currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 
 }
--- src/test/MoneyTest.java
+++ src/test/MoneyTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import money.Bank;
+import money.Expression;
 import money.Money;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +27,15 @@ public class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 
 }
